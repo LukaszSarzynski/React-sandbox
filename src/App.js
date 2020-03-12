c43d24233f082d4a20c4510c9b7f50065599b3e7
LukaszSarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2020-03-12 21:52:12 +0100

    add child input componet

--- src/App.js
+++ src/App.js
@@ -20,11 +20,18 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <input type="text" onChange={this.update.bind(this)} /> <br/>
-        <strong>{this.state.message}, you changed input {this.state.count} times</strong>
+        <h2>{this.state.message}, you changed input {this.state.count} times</h2> 
+        <InputWidget update={this.update.bind(this)} /> <br/>
+        <InputWidget update={this.update.bind(this)} /> <br/>
+        <InputWidget update={this.update.bind(this)} /> <br/>
+        <InputWidget update={this.update.bind(this)} /> <br/>
       </div>
     )
   }
 }
+
+const InputWidget = (props) => 
+  <input type="text" onChange={props.update} /> 
+
 
 export default App;
