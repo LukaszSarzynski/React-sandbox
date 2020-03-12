d69bc93d5f7d0fc4b33dda16914a06ffa8cd2550
LukaszSarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2020-03-12 21:24:42 +0100

    Properties fun

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 
 class App extends React.Component {
 
   render() {
-    //return React.createElement('h1',null, 'hello from App componet without JSX');
-    return <h1>hello from App componet</h1>
+    return (
+      <div>
+        <strong>Hey {this.props.name}, your number is {this.props.count}</strong>
+      </div>
+    )
   }
+}
+
+App.propTypes = {
+  count: PropTypes.number.isRequired,
+  name: PropTypes.string,
+}
+
+App.defaultProps = {
+  name: 'No-name',
 }
 
 export default App;
