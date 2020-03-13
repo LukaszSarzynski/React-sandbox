090b64314c61bac7a3bcb14519750c4a38b974a5
LukaszSarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2020-03-13 22:04:40 +0100

    this.props.children trick to extended data

--- src/App.js
+++ src/App.js
@@ -2,57 +2,49 @@ import React from 'react';
 
 class App extends React.Component {
 
+  
+  render() {
+    
+    return (
+      <ChildDirection>
+        <p>-up-</p>
+        <span>-left-</span>
+        <span>-right-</span>
+        <p>-down-</p>
+      </ChildDirection>
+    )
+  }
+}
+// <span>left</span> <span>right</span>
+
+class ChildDirection extends React.Component {
+
   constructor() {
     super();
 
     this.state = {
-      users: [1,2],
+      direction: '--'
     }
   }
 
-  UNSAFE_componentWillMount() {
-    fetch( 'https://jsonplaceholder.typicode.com/users' )
-    .then( response => response.json() )
-    .then( json => this.setState({users: json}))
-  }
-
-  setFilter(e) {
-    this.setState({filter: e.target.value})
+  changeDirection(direction) {
+    this.setState({direction});
   }
 
   render() {
-    let users = this.state.users;
-    if(this.state.filter) {
-      users = users.filter( v => v.name.includes(this.state.filter));
-    }
-    console.log(this.state.filter);
+    let children = this.props.children
+    children = React.Children.map(children, child => {
+      return React.cloneElement(child, {onMouseOver: this.changeDirection.bind(this,child.props.children)})
+    })
     return (
       <div>
-        <input onChange={this.setFilter.bind(this)} />  
-        {users.map( (v,i) => <UserWidget key={i} user={v} /> )}
+        <h1>Go to {this.state.direction}</h1>
+        {children}
       </div>
     )
   }
 }
 
-const UserWidget = (props) =>
-      <div>
-        <h2>{props.user.name}</h2>
-        <p><strong>username: </strong>{props.user.username}</p>      
-        <p><strong>email: </strong>{props.user.email}</p>      
-      </div>
 
-
-// class UserWidget extends React.Component {
-//   render(){
-//     return (
-//       <div>
-//         <h2>{this.props.user.name}</h2>
-//         <p><strong>username: </strong>{this.props.user.username}</p>      
-//         <p><strong>email: </strong>{this.props.user.email}</p>      
-//       </div>
-//     )
-//   }
-// }
 
 export default App;
